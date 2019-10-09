6a76e086022a9d34da8bab0b28fde36063e2c961
MaisemShah <54338906+MaisemShah@users.noreply.github.com>
2019-10-09 12:49:58 -0500

    Add files via upload

---
+++ Cars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CArsssss.Data
+{
+    public class Cars
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CID { get; set; }
+        [Display(Name = "Mustang")]
+        public string Mustang { get; set; }
+        [Display(Name = "Vantage")]
+
+        public string Vantage { get; set; }
+        [Display(Name = "Corvette")]
+        public string Corvette { get; set; }
+
+    }
+}
---
+++ Manufacter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CArsssss.Data
+{
+    public class Manufacter
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int  MID { get; set; }
+        public string Ford { get; set; }
+
+        public string AStonMartin { get; set; }
+
+        public string Cheverolet { get; set; }
+        [ForeignKey("CID")]
+        public Cars CarsID { get; set; }
+    }
+}
